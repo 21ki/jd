0b652c65ab40f3772d07f51c050661f9818acd69
Myki <21kixc@gmail.com>
2021-11-08 17:12:51 +0800

    Update picture_random.py

--- scripts/python/picture_random.py
+++ scripts/python/picture_random.py
@@ -1,10 +1,17 @@
 import random
-list1 = ['2fwYgGVEyesz-VbIw9RLJww3UCA3BDFbecoCC8PVIajB9WZ0UKuuYND1h95TeeV3d',
-         '22z1AOYS_3WZrx8qRf-OY48GQ5fpQ7dZh53cCycVF_FjRgl2BTUTCVIM6uFKGgubS',
-         '2yEzS1yVa3uQKRLlJDh8RZShvSpl31deC-nt09sgGdOZbdnISJCvd0WfLWOK4VJkM',
-         '2Kxhc7nYNufv0tbhzEjrGsjgdoXfjjVCkdErovRoMt3sN_zItVrZezvgJW38hAJsy',
-         '2uQGhzWlmZYKbCUJzAMESf9bsZlGHR9TK-vD6PA3qliW3UkEfIXc2YB_a971Yc9Md',
-         '2JH178Jl6FDZ7BrEKBg5n0OiwTuWfvZp9VMxI_Gqw4ZXDndyrldQbVEm4FMnqMZ8s',
-         '2yroULnZ-4paoA_MJx8DbYtEIcKznOeya79x98S178CmSeqmMjYydekxd6m9sF-71']
+
+list1 = [
+    '2fwYgGVEyesz-VbIw9RLJww3UCA3BDFbecoCC8PVIajB9WZ0UKuuYND1h95TeeV3d',
+    '22z1AOYS_3WZrx8qRf-OY48GQ5fpQ7dZh53cCycVF_FjRgl2BTUTCVIM6uFKGgubS',
+    '2yEzS1yVa3uQKRLlJDh8RZShvSpl31deC-nt09sgGdOZbdnISJCvd0WfLWOK4VJkM',
+    '2Kxhc7nYNufv0tbhzEjrGsjgdoXfjjVCkdErovRoMt3sN_zItVrZezvgJW38hAJsy',
+    '2uQGhzWlmZYKbCUJzAMESf9bsZlGHR9TK-vD6PA3qliW3UkEfIXc2YB_a971Yc9Md',
+    '2JH178Jl6FDZ7BrEKBg5n0OiwTuWfvZp9VMxI_Gqw4ZXDndyrldQbVEm4FMnqMZ8s',
+    '2jtdZh2tPfyVf5gzKsgMZvkS0AzORTKZV8-1XJja1lIKNllhvzpdpWi8FdgwDC7be',
+    '2R-KYgbkm9L9WsmjMd_w1Jrrpd5TM3jnr69K0xEz7W_P_olpBsx9q71i-9BGaBgak',
+    '2AdNxwZgnY8FyKOfNx4qJjmfAzlAWfL6ITEI8uVu2UwW3BdQ4kTgDaEmQM2jJ3gDG',
+    '2tBAPbCvgUU8X4SCJfRgGMHhK6U-01-B4rR6LiSTuR9soxStZdlGfgjgp0ptss_Wt',
+    '2Ayq1ZgKkkUitlgDQkrLyBSApdXeIcX5J5CQeATQVDAOD--PkXRh4IaSOd2aGOFRK'
+]
 a = random.choice(list1)
 print(a)
