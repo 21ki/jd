5128f3d788b578371a068b9d710cf0f8ef5f01f1
Myki <21kixc@gmail.com>
2021-11-08 15:10:23 +0800

    Create picture_random.py

---
+++ scripts/python/picture_random.py
@@ -0,0 +1,10 @@
+import random
+list1 = ['2fwYgGVEyesz-VbIw9RLJww3UCA3BDFbecoCC8PVIajB9WZ0UKuuYND1h95TeeV3d',
+         '22z1AOYS_3WZrx8qRf-OY48GQ5fpQ7dZh53cCycVF_FjRgl2BTUTCVIM6uFKGgubS',
+         '2yEzS1yVa3uQKRLlJDh8RZShvSpl31deC-nt09sgGdOZbdnISJCvd0WfLWOK4VJkM',
+         '2Kxhc7nYNufv0tbhzEjrGsjgdoXfjjVCkdErovRoMt3sN_zItVrZezvgJW38hAJsy',
+         '2uQGhzWlmZYKbCUJzAMESf9bsZlGHR9TK-vD6PA3qliW3UkEfIXc2YB_a971Yc9Md',
+         '2JH178Jl6FDZ7BrEKBg5n0OiwTuWfvZp9VMxI_Gqw4ZXDndyrldQbVEm4FMnqMZ8s',
+         '2yroULnZ-4paoA_MJx8DbYtEIcKznOeya79x98S178CmSeqmMjYydekxd6m9sF-71']
+a = random.choice(list1)
+print(a)
