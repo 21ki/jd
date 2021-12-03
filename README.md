6490e3ee455387046fba43f057ce52c960a4dde6
21ki <21kixc@gmail.com>
2021-12-03 17:35:55 +0800

    get_ip_public

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## dev自用备份脚本
 ```
-ql repo https://ghproxy.com/https://github.com/21ki/jd.git "jd_jxz|jd_debug_env|jd_jcqd|jd_Evaluation|jd_jkd"
+ql repo https://ghproxy.com/https://github.com/21ki/jd.git "jd_jxz|jd_debug_env|jd_Evaluation|jd_jkd|get_ip_public"
 
 ```
 #jd_jxz集勋章
---
+++ scripts/python/get_ip_public.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 _*-
+"""
+@Author:Myki
+@Blog(个人博客地址): https://www.1nth.com
+@WeChat Official Account(微信公众号)：MykiFan
+@Github:www.github.com  https://github.com/21ki
+cron: 30 9 * * *
+new Env('debuenv');
+"""
+
+import socket
+import requests
+import re
+
+title = ''
+content = ''
+
+class IP:
+    @staticmethod
+    def get_ip_public():
+        """
+        获取本机公网IP
+        :return:
+        """
+        try:
+            text = requests.get("http://txt.go.sohu.com/ip/soip").text
+            ip_public = re.findall(r'\d+.\d+.\d+.\d+', text)[0]
+            return ip_public
+        except:
+            return '0.0.0.0'
+
+    @staticmethod
+    def get_ip_local():
+        """
+        查询本机内网IP
+        :return:
+        """
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('8.8.8.8', 80))
+            ip = s.getsockname()[0]
+        finally:
+            s.close()
+
+        return ip
+
+
+if __name__ == '__main__':
+    print("内网IP：{}".format(IP.get_ip_local()))
+    print("外网IP：{}".format(IP.get_ip_public()))
+    title= "get_ip_public"
+    content = IP.get_ip_public()
